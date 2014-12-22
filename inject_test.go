1f80ff16f3eb2c91895836845d99ed47cf52b571
Peter Edge <peter.edge@gmail.com>
2014-12-22 17:52:02 -0600

    first testing

--- inject_test.go
+++ inject_test.go
@@ -1,6 +1,42 @@
 package inject
 
 import (
-//"github.com/stretchr/testify/require"
-//"testing"
+	"github.com/stretchr/testify/require"
+	"testing"
 )
+
+type SimpleInterface interface {
+	Foo() string
+}
+
+type SimpleStruct struct {
+	foo string
+}
+
+func (this SimpleStruct) Foo() string {
+	return this.foo
+}
+
+type SimplePtrStruct struct {
+	foo string
+}
+
+func (this *SimplePtrStruct) Foo() string {
+	return this.foo
+}
+
+func TestSimpleStructSingletonDirect(t *testing.T) {
+	injector := CreateInjector()
+	err := injector.Bind((*SimpleInterface)(nil)).ToSingleton(SimpleStruct{"hello"})
+	requireErrNil(t, err)
+	//container, err := injector.CreateContainer()
+	_, err = injector.CreateContainer()
+	requireErrNil(t, err)
+}
+
+// TODO(pedge): is there something for this in testify? if not, send a pull request
+func requireErrNil(t *testing.T, err error) {
+	if err != nil {
+		require.Nil(t, err, err.Error())
+	}
+}
