ba65dcd7776cb761e84113c4b86f7c8c1a4f5f49
Peter Edge <peter.edge@gmail.com>
2014-12-22 17:56:30 -0600

    more testing

--- inject_test.go
+++ inject_test.go
@@ -29,9 +29,12 @@ func TestSimpleStructSingletonDirect(t *testing.T) {
 	injector := CreateInjector()
 	err := injector.Bind((*SimpleInterface)(nil)).ToSingleton(SimpleStruct{"hello"})
 	requireErrNil(t, err)
-	//container, err := injector.CreateContainer()
-	_, err = injector.CreateContainer()
+	container, err := injector.CreateContainer()
 	requireErrNil(t, err)
+	object, err := container.Get((*SimpleInterface)(nil))
+	requireErrNil(t, err)
+	simpleInterface := object.(SimpleInterface)
+	require.Equal(t, "hello", simpleInterface.Foo())
 }
 
 // TODO(pedge): is there something for this in testify? if not, send a pull request
