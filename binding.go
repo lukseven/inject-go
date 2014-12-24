6e29d14ac12ac20c0c681925acd93d16fdc12a90
Peter Edge <peter.edge@gmail.com>
2014-12-24 15:11:25 -0600

    comments

--- binding.go
+++ binding.go
@@ -11,6 +11,9 @@ const (
 )
 
 type binding interface {
+	// has to be a copy constructor
+	// https://github.com/peter-edge/inject/commit/e525825afc80f0de819f35a6afc26a4bf3d3a192
+	// this could be designed better
 	resolvedBinding(*module, *injector) (resolvedBinding, error)
 }
 
