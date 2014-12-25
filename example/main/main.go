3b73077bd345ea6007b95606932e8a722823f938
Peter Edge <peter.edge@gmail.com>
2014-12-25 02:24:39 -0600

    main.go in example

---
+++ example/main/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"gopkg.in/peter-edge/inject.v1"
+	"gopkg.in/peter-edge/inject.v1/example/api"
+	"gopkg.in/peter-edge/inject.v1/example/cloud"
+	"gopkg.in/peter-edge/inject.v1/example/more"
+	"gopkg.in/peter-edge/inject.v1/example/stuff"
+	"os"
+)
+
+func main() {
+	injector, err := inject.CreateInjector(
+		api.CreateModule(),
+		cloud.CreateModule(),
+		more.CreateModule(),
+		stuff.CreateModule(),
+	)
+	if err != nil {
+		panic(err)
+	}
+	obj, err := injector.Get((*api.Api)(nil))
+	if err != nil {
+		panic(err)
+	}
+	api := obj.(api.Api)
+	response, err := api.Do(api.Request{os.Args[1], "this is fun"})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v %v\n", response.bar, response.baz)
+}
